83edc5b936fa608a331e6dddfdf31ef27b5fcccc
Tiagorn <trdsnpereira@restinga.ifrs.edu.br>
2021-07-28 01:04:09 -0300

    Usuario

---
+++ src/Usuario/ProdutoForm.js
@@ -0,0 +1,345 @@
+import React from 'react';
+import axios from 'axios';
+import FormBotoes from '../SisPadrao/FormBotoes'
+import FormBotoesDetalhe from '../SisPadrao/FormBotoesDetalhe'
+
+import SisMensagemView from '../SisPadrao/SisMensagemView';
+import SisManterView from '../SisPadrao/SisManterView';
+
+class ProdutoForm extends React.Component
+{
+    constructor(props)
+    {        
+        super(props);
+        if(this.props.entidade.id==0)
+        {
+            this.state={
+                 id:0
+                ,nome:''
+                ,classeId:0
+                ,visao:process.env.REACT_APP_VISAO_INFORMANDO
+                ,mensagens:null
+                ,listaBuscou:false
+            };
+        }
+        else
+        {             
+            this.state={
+                id:this.props.entidade.id
+               ,visao:'processando'
+               ,mensagens:null
+               ,listaBuscou:false
+           };
+           let entidade = {id:this.props.entidade.id}
+
+           this.SisManterConsultar(entidade);
+        }
+
+        this.Listar();
+    }
+        
+    Salvar()
+    {
+        let entidade =  {
+        id:this.state.id
+        ,nome:this.state.nome
+        ,classe:{id:this.state.classeId}
+        };
+        this.SisManterSalvar(entidade);
+    }
+
+    
+    Salvou(resposta)
+    {        
+        var retorno = null;
+
+        if(resposta.request.status == 200)
+        {
+            var erro = resposta.data.erro;
+            if(erro != null)
+            {
+                var itens = erro.itens;
+                var msg = itens[0].mensagem;
+                retorno = {visao:"mensagem.erro"
+                  , mensagens:erro.itens
+                };
+            }
+            else
+            {
+                retorno = {visao:"mensagem.sucesso"
+                  ,mensagens:window.ToMensagens("Registro salvo com sucesso.")
+                };
+            }
+        }
+        else
+        {
+            retorno = {visao:"mensagem.erro"
+            ,mensagens:window.ToMensagens("Erro ao salvar registro, repita a operação.")
+            };
+        }
+
+        this.Evento(retorno, 'salvou');
+
+    }
+
+
+    Excluir()
+    {
+        let entidade =  {
+        id:this.state.id
+        };
+        this.SisManterExcluir(entidade);
+    }
+
+
+
+    SisManterExcluir(entidade)
+    {
+        this.setState({visao:'processando'});
+
+        axios.get(process.env.REACT_APP_SERVER_URL + "/api/produto/excluir/" + entidade.id
+            ,window.getCabeca())
+            .then((resposta)=>this.Excluiu(resposta))
+            .catch((resposta => this.Excluiu(resposta))
+            );
+
+    }
+
+    Excluiu(resposta)
+    {      
+        var retorno = null;
+
+        if(resposta.request.status == 200)
+        {
+            var erro = resposta.data.erro;
+            if(erro != null)
+            {
+                var itens = erro.itens;
+                var msg = itens[0].mensagem;
+                retorno = {visao:"mensagem.erro"
+                  , mensagens:erro.itens
+                };
+            }
+            else
+            {
+                retorno = {visao:"mensagem.sucesso"
+                  ,mensagens:window.ToMensagens("Registro excluido com sucesso.")
+                };
+            }
+        }
+        else
+        {
+            retorno = {visao:"mensagem.erro"
+            ,mensagens:window.ToMensagens("Erro ao excluir registro, repita a operação.")
+            };
+        }
+    
+        this.Evento(retorno, 'excluiu');
+}
+
+    Evento(resposta, acao)
+    {        
+        if(acao=='pesquisou' 
+            || acao=='consultou'
+            || acao=='salvou'
+            || acao=='excluiu'
+            )
+        {
+        }
+
+
+        if(acao=='salvou')
+        {
+            this.setState({visao:resposta.visao, mensagens:resposta.mensagens});
+        }
+        else if(acao =='consultou')
+        {
+            let estado={
+                id:resposta.entidade.id
+               ,nome:resposta.entidade.nome
+               ,classeId:resposta.entidade.classe.id
+               ,visao:process.env.REACT_APP_VISAO_INFORMANDO
+            };
+            this.setState(estado);
+        }
+        else if(acao =='excluiu')
+        {
+            this.setState({visao:resposta.visao, mensagens:resposta.mensagens});
+        }
+    }
+
+    SisManterSalvar(entidade)
+    {
+        this.setState({visao:'processando'});
+
+        if(entidade.id==0)
+        {
+            axios.post(process.env.REACT_APP_SERVER_URL + "/api/produto/salvar"
+                ,entidade
+                ,window.getCabeca()
+            )
+            .then((resposta)=>this.Salvou(resposta))
+            .catch((resposta) => this.Salvou(resposta));
+        }
+        else
+        {
+            axios.post(process.env.REACT_APP_SERVER_URL + "/api/produto/salvar"
+                ,entidade
+                ,window.getCabeca()
+            )
+            .then((resposta)=>this.Salvou(resposta))
+            .catch((resposta) => this.Salvou(resposta));
+        }
+    }
+
+    SisManterConsultar(entidade)
+    {
+
+        axios.get(process.env.REACT_APP_SERVER_URL + "/api/produto/consultar/" + entidade.id
+            ,window.getCabeca()
+            )
+            .then((resposta)=>this.Consultou(resposta))
+            .catch((resposta) => this.Consultou(resposta));
+    }
+
+    Consultou(resposta)
+    {
+        var retorno = null;
+
+        if(resposta.status == 200)
+        {   
+            var erro = resposta.data.erro;
+            if(erro != null)
+            {
+                var itens = erro.itens;
+                var msg = itens[0].mensagem;
+                retorno = {visao:"mensagem.erro"
+                  , mensagens:erro.itens
+                };
+            }
+            else
+            {
+                retorno = {visao:"consultar"
+                    ,entidade:resposta.data.dados
+                    ,entidadePesquisa:resposta.data.dados
+                    ,mensagens:window.ToMensagens("Consulta retornou com sucesso.")
+                };
+            }
+            
+        }
+        else
+        {
+            retorno = {visao:"mensagem.erro"
+            ,mensagens:window.ToMensagens("Erro ao consultar registro, repita a operação.")
+            };
+        }
+
+        this.Evento(retorno, 'consultou');
+    }
+
+    Listar()
+    {
+        axios.get(process.env.REACT_APP_SERVER_URL + "/api/classe/listar",window.getCabeca()).then((resposta)=>this.Listou('classe',resposta));
+    }
+
+    Listou(tipo, resposta)
+    {
+        if(tipo=='classe')
+        {
+            if(resposta.request.status == 200)
+            {
+                this.setState({lista:resposta.data.dadosLista, listaBuscou:true});
+            }
+    
+        }
+    }
+
+
+    render()
+    {
+        return(
+
+<div class="card">
+  <div class="card-header">
+    {this.state.id == 0 ? "I N C L U I R" : "A L T E R A R"}
+  </div>
+  <div class="card-body">
+
+  <fieldset>
+
+        <div class="form-group">
+
+            <input type="text" class="form-control" id="inputNome"  
+                aria-describedby="nomeHelp" 
+                placeHolder="Informe o nome." 
+                onChange={(o)=>this.setState({nome:o.target.value})}
+                value={this.state.nome}
+            />
+
+            {this.state.listaBuscou==true ?
+                <select 
+                    class="form-control form-control-sm" 
+                    id="InputClasse" 
+                    onChange={(o)=>this.setState({classeId:o.target.value})}
+                    defaultValue={this.state.classeId}
+                    value={this.state.classeId}
+                >
+            
+                {this.state.lista != null ?
+
+                    this.state.lista.map( (entidade) =>
+                    <option 
+                        value={entidade.id} 
+                        >{entidade.nome}</option> 
+                    )
+                : ""
+                }
+                <option value="0" >Informe a classe</option>
+
+                </select>
+                :
+                <div></div>
+            }
+
+    
+            <SisMensagemView
+                visao={this.state.visao}
+                mensagens={this.state.mensagens}
+                OnClicou={(v) => this.setState({visao:v})}
+            />
+
+            {this.state.visao=='processando' ?
+                <div class="text-center">
+                    <div class="spinner-border" role="status">
+                    </div>
+                </div>
+            : ""
+            }
+
+        </div>
+
+        <FormBotoes
+            id={this.props.entidade.id} 
+            listaAutorizacao={this.props.listaAutorizacao}
+            objetoAutorizacao={this.props.objetoAutorizacao}
+            processando={this.props.processando}
+            OnExcluir={() => this.Excluir()}
+            OnSalvar={() => this.Salvar()}
+            OnVoltar={() => this.props.OnVoltar()}
+        />
+
+
+    </fieldset>
+
+  </div>
+</div>
+                            
+        );
+    
+    }
+
+}
+
+
+export default ProdutoForm;
+
